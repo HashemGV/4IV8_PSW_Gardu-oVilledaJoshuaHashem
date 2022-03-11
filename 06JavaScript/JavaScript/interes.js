e3181874b3123cf015543d4483c13921bf17b007
HashemGV <jgardunov2001@alumno.ipn.mx>
2022-03-10 20:37:24 -0600

    ejercicio JS

--- 06JavaScript/JavaScript/interes.js
+++ 06JavaScript/JavaScript/interes.js
@@ -1,10 +1,18 @@
-function validar(e){
-    teclado=(document.all)?e.keyCode:e.w;
+function validarn(e){
+    var teclado = (document.all)?e.KeyCode:e.wich;
     /*vaqmos a aplicar una prueba logica a todo el teclado a partir
     de un patrón, este patrón debe de ser numérico y solo acepta
     del 0 a 9. Todo lo demas lo deja escapar*/
-    if(Teclado==8)return true;
-    var patrón=/[0_9\d .]/;
-    var probar=String.fromCharCode(teclado);
-    return probar.test(probar)
+    if(teclado == 8) return true;
+    var patron = /[0-9\d .]/;
+    var probar = String.fromCharCode(teclado);
+    return patron.test(probar);
+}
+
+function interes(){
+    var valor = document.formulario1.cantidad.value;
+    var resul = parseInt(valor);
+    var interes = resul*0.025;
+    var total = resul+interes;
+    document.formulario1.sueldoI.value = "$" + total;
 }
