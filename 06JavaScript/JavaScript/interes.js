2142d3ec003bb9e812c2f75124678bba5643ca9d
HashemGV <jgardunov2001@alumno.ipn.mx>
2022-03-14 19:16:42 -0600

    Script

--- 06JavaScript/JavaScript/interes.js
+++ 06JavaScript/JavaScript/interes.js
@@ -1,20 +1,20 @@
 function validarn(e){
-    var teclado = (document.all)?e.KeyCode:e.wich;
+    var teclado=(document.all)?e.KeyCode:e.wich;
     /*vaqmos a aplicar una prueba logica a todo el teclado a partir
     de un patrón, este patrón debe de ser numérico y solo acepta
     del 0 a 9. Todo lo demas lo deja escapar*/
-    if(teclado == 8) return true;
-    var patron = /[0-9\d .]/;
-    var probar = String.fromCharCode(teclado);
+    if(teclado==8) return true;
+    var patron=/[0-9\d .]/;
+    var probar=String.fromCharCode(teclado);
     return patron.test(probar);
 }
 
 function interes(){
-    var valor = document.formulario1.cantidad.value;
-    var resul = parseInt(valor);
-    var interes = resul*0.025;
-    var total = resul+interes;
-    document.formulario1.sueldoI.value = "$" + total;
+    var valor=document.formulario1.cantidad.value;
+    var resul=parseInt(valor);
+    var interes=resul*0.025;
+    var total=resul+interes;
+    document.formulario1.sueldoI.value= "$" + total;
 }
 
 function borrar(){
