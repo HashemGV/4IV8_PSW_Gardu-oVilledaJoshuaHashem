75badd90e6098c59543c941a4e1aaead8870d535
HashemGV <jgardunov2001@alumno.ipn.mx>
2022-03-15 23:15:48 -0600

    El return no sirve

--- 06JavaScript/JavaScript/interes.js
+++ 06JavaScript/JavaScript/interes.js
@@ -1,23 +1,27 @@
 function validarn(e){
-    var teclado=(document.all)?e.KeyCode:e.wich;
-    /*vaqmos a aplicar una prueba logica a todo el teclado a partir
-    de un patrón, este patrón debe de ser numérico y solo acepta
-    del 0 a 9. Todo lo demas lo deja escapar*/
+    var teclado = (document.all)?e.KeyCode : e.which;
+    /* vamos aplicar una prueba logica a todo el teclado
+    a partir de un patron este patron debe de ser
+    numerico y solo acepta 0-9 . todo lo demas lo deja
+    escapar */
     if(teclado==8) return true;
-    var patron=/[0-9\d .]/;
-    var probar=String.fromCharCode(teclado);
+    var patron = /[0-9\d .]/;
+    var probar = String.fromCharCode(teclado);
     return patron.test(probar);
 }
 
+
 function interes(){
-    var valor=document.formulario1.cantidad.value;
-    var resul=parseInt(valor);
-    var interes=resul*0.025;
-    var total=resul+interes;
-    document.formulario1.sueldoI.value= "$" + total;
+    var valor = document.formulario1.cantidad.value;
+    //tipo de dato
+    var resul = parseInt(valor);
+    var interes = resul*0.025;
+    var total = resul + interes;
+    //imprimir el resultado
+    document.formulario1.sueldoI.value = "$" + total;
 }
 
-function borrar(){
-    document.formulario1.sueldoI.value="";
-    
+function borrard(){
+    document.formulario1.sueldoI.value = "";
+    document.formulario1.cantidad.value = "";
 }
