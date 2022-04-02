102d0509b953a5f81e92ea15edf0f49288ba62ff
HashemGV <jgardunov2001@alumno.pin.mx>
2022-04-01 22:52:32 -0600

    osiosi

--- 10BD/web/JS/validacion.js
+++ 10BD/web/JS/validacion.js
@@ -5,3 +5,19 @@ function validar(e){
     var probar=String.fromCharCode(teclado);
     return patron.test(probar);
 }
+
+ function letras(e){
+    var nom=document.formulario.nombre.value;
+    var appat=document.formulario.appat.value;
+    var apmat=document.formulario.apmat.value;
+    var bol=document.formulario.boleta.value;
+    var tel=document.formulario.telefono.value;
+    if((nom=="")||(appat=="")||(apmat=="")||(bol=="")||(tel=="")){
+        alert("Debes llenar todos los campos")
+    }
+    else if (!str.search("/[A-Z][a-z]+\s/",nom)&&!str.search("/[A-Z][a-z]+\s/",appat)&&
+    !str.search("/[A-Z][a-z]+\s/",apmat)&&!str.search("\d\d\d\d\d\d\d\d\d\d",bol)&&
+    !str.search("\d\d\d\d\d\d\d\d\d\d",tel)){
+        alert("Datos invalidos")
+    }
+ }
